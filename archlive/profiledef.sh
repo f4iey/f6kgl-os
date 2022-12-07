dda07b35880f88120220e0501e464664fe3cf7e0
f4iey <benkemounjules@gmail.com>
2022-12-07 01:44:01 +0100

    update to v5

--- archlive/profiledef.sh
+++ archlive/profiledef.sh
@@ -2,13 +2,15 @@
 # shellcheck disable=SC2034
 
 iso_name="f6kgl"
-iso_label="f6kgl-v4.8"
+iso_label="f6kgl-v5"
 iso_publisher="F6KGL OS <https://f6kgl-f5kff.fr>"
 iso_application="F6KGL OS Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
@@ -21,7 +23,6 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
-  ["/etc/pacman.d/mirrorlist"]="0:0:755"
   ["/etc/skel/Desktop/sdrpp.desktop"]="0:0:755"
   ["/etc/skel/Desktop/klog.desktop"]="0:0:755"
   ["/etc/skel/Desktop/qsstv.desktop"]="0:0:755"
